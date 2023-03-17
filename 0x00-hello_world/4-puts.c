68bed421b25fc98379d5438be739a37dcc7c22e0
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-16 17:54:26 -0700

    fix - betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/*
+ * program prints a string and return true upon successful execution
+ *
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
