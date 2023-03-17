8e121789e61e9157fc4b4fc2a8a9fdfe341f5ac4
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-16 17:57:46 -0700

    fix betty comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- * program prints a string and return true upon successful execution
- *
-*/
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
