0c4113a76fb45d47ccfe7ae040d49530f579d106
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-21 05:43:01 -0700

    fixed modulo operator to accomodate negative #

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,17 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last_digit;
+
+	if (n > 0)
+	{
+		last_digit = n % 10;
+		_putchar('0' + last_digit);
+	}
+	else
+	{
+		last_digit = -n % 10;
+		_putchar('0' + last_digit);
+	}
+	return (last_digit);
 }
