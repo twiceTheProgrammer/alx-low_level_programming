3ab99a94f4081e2d53c23800d78eaaaf44b2c650
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-21 03:25:59 -0700

    added readme & associated files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar followed by new line
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('a');
+	_putchar('\n');
 	return (0);
 }
