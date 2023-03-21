ca1ab4b7a56158de3b636656e76f6fd87b8d68cd
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-21 08:14:31 -0700

    fixed end of line & int output format

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,15 +14,20 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
 		}
-		_putchar('\n');
 	}
 	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			printf("%d, ", j);
+			if (j != 98)
+				printf("%d, ", j);
+			else
+				printf("%d\n", j);
 		}
 		_putchar('\n');
 	}
