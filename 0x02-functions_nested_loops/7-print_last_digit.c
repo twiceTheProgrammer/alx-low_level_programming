696bd980ee8f39c096bcb232906dbab07cb156a7
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-21 05:50:03 -0700

    fixed negative last_digit return

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,14 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n > 0)
-	{
-		last_digit = n % 10;
-		_putchar('0' + last_digit);
-	}
-	else
-	{
-		last_digit = -n % 10;
-		_putchar('0' + last_digit);
-	}
+	if (n < 0)
+		n = -n;
+
+	last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit = -last_digit;
+
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
