a78ef9cd380ab5b21af0612a9c0cd46af2b62f66
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-03-21 03:57:29 -0700

    print 10 row of lowercase alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 rows of alphabets followed by new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	while (count < 10)
+	{
+		print_alphabet();
+		count++;
+	}
+}
