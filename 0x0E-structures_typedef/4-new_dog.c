07c0ac7428a8f9c938f932565ef9ca3bad85d062
Twice <njabulo.magagula@ribccs.com>
2023-04-17 22:49:55 +0200

    create new dog & store copy

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * *new_dog - creates new dog struct
+ * @name: param
+ * @age: param
+ * @owner: param
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(strlen(name) + 1);
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc(strlen(owner)+1);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+
+	new_dog->age = age;
+
+	return new_dog;
+}
